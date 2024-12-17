ed15401777cb7809f5ea5b14be8a253b6d6feed1
aniket4518 <jhaaniket2005@gmail.com>
2024-12-18 03:05:11 +0530

    this is version 2

--- todo-app/todoappv1/src/App.jsx
+++ todo-app/todoappv1/src/App.jsx
@@ -51,7 +51,7 @@ const handeldelete=(itemname)=>{
             <div className="col"> 
             <button type="button" className="btn btn-danger">Delete</button>
             </div>
-  </div>
+  </div>//ass
  
 </div>   */}
 </Container>
